4c6aa3d560ac3e464fe35ea7654d122bd2e2461c
Asya404 <unkiplum@gmail.com>
2022-03-23 16:24:34 +0100

    read values from the form; error message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 const eventbrite = new EventBrite();
+const btn = document.querySelector('#submitBtn');
+
+// on submit read values and print the result
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // read the values
+    const eventName = document.querySelector('#event-name').value;
+    const category = document.querySelector('#category').value;
+
+    // validating and print the result or error
+    if (eventName !== '') {
+        // cryptoapi.requestAPI(currencySelect, cryptoSelect)
+        //     .then(data => {
+        //         data = data.data;
+        //         cryptoapi.displayResult(data, currencySelect);
+        //     })
+    } else {
+        eventbrite.printMessage('Add an Event or City', 'error');
+    }
+})
--- js/classes.js
+++ js/classes.js
@@ -6,6 +6,7 @@ class EventBrite {
         this.printCrypto();
     }
 
+
     // Here we get data with all the crypto, then build the options
     async getCategories() {
         const response = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.auth_token}`)
@@ -14,6 +15,7 @@ class EventBrite {
             data
         }
     }
+
 
     // Build the options in select
     printCrypto() {
@@ -30,5 +32,20 @@ class EventBrite {
                 console.log(categoriesList);
             })
             .catch(error => console.log(error));
+    }
+
+
+    // Prints an error message
+    printMessage(message, className) {
+        const div = document.createElement('div');
+        div.className = className;
+        div.textContent = message;
+        const messageDiv = document.querySelector('.messages');
+        messageDiv.appendChild(div);
+
+        // Remove the message
+        setTimeout(() => {
+            document.querySelector('.messages div').remove();
+        }, 2000)
     }
 }
